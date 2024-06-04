85a51cb76f743ef0b78b90b4f194ad905e6e4dd4
Ray <rayc12079@gmail.com>
2024-06-04 16:38:47 -0400

    Updated hero image sizes

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -9,8 +9,8 @@ export default function Hero() {
   return (
     <section className="grid md:grid-cols-10 grid-cols-1 items-center gap-5">
       <article className="space-y-3 col-span-6">
-        <h1 className="font-bold md:text-5xl text-[32px] leading-9">
-          Ask Anonymously, <br /> Grow Confidently!
+        <h1 className="font-bold md:text-4xl text-[32px] leading-9">
+          Ask Anonymously, Grow <br /> Confidently!
         </h1>
         <Button variant="primary" asChild>
           <Link href="/rooms">
@@ -18,15 +18,8 @@ export default function Hero() {
           </Link>
         </Button>
       </article>
-      <section className="col-span-4">
-        <Image
-          src="/hero.svg"
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Hero Image"
-          className="w-full h-auto"
-        />
+      <section className="col-span-4 flex md:justify-end justify-start">
+        <Image src="/hero.svg" width={400} height={400} alt="Hero Image" />
       </section>
     </section>
   );
--- src/app/about/_components/AboutHero.jsx
+++ src/app/about/_components/AboutHero.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function AboutHero() {
   return (
-    <section className="grid md:grid-cols-10 grid-cols-1 items-center gap-5">
-      <article className="col-span-7">
+    <section className="grid md:grid-cols-10 grid-cols-1 items-center gap-5 mb-10">
+      <article className="col-span-6">
         <div className="max-w-[600px] space-y-2">
-          <h1 className="font-bold md:text-5xl text-[32px] leading-9">
+          <h1 className="font-bold md:text-4xl text-[32px] leading-9">
             Our Mission:
           </h1>
           <p>
@@ -19,17 +19,14 @@ export default function AboutHero() {
           </p>
         </div>
       </article>
-      <section className="col-span-3">
+      <section className="col-span-4 flex md:justify-end justify-start">
         <Image
           src="/about-hero.svg"
-          width={0}
-          height={0}
-          sizes="100vw"
+          width={400}
+          height={400}
           alt="Hero Image"
-          className="w-full h-auto"
         />
       </section>
-      <br />
     </section>
   );
 }
