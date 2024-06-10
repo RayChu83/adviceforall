ceeef24fe26959a71ffaab7ddf6904429795614c
Ray <rayc12079@gmail.com>
2024-06-09 21:58:55 -0400

    Fixed search filtering bug

--- src/app/_components/RoomsContainer.jsx
+++ src/app/_components/RoomsContainer.jsx
@@ -8,22 +8,20 @@ import React from "react";
 export default function RoomsContainer({ rooms }) {
   return (
     <section className="grid lg:grid-cols-9 sm:grid-cols-6 grid-cols-1 items-start gap-5">
-      <AnimatePresence>
-        {rooms.map((room, index) => (
-          <motion.div
-            key={index}
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={GRADUAL}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-            viewport={{ once: true }}
-            className="col-span-3 bg-blue-darker drop-shadow-md rounded-md overflow-hidden cursor-pointer group"
-          >
-            <Room room={room} />
-          </motion.div>
-        ))}
-      </AnimatePresence>
+      {rooms.map((room, index) => (
+        <motion.div
+          key={index}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={GRADUAL}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+          viewport={{ once: true }}
+          className="col-span-3 bg-blue-darker drop-shadow-md rounded-md overflow-hidden cursor-pointer group"
+        >
+          <Room room={room} />
+        </motion.div>
+      ))}
     </section>
   );
 }
