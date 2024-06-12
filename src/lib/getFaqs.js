ceac484fd838143cc5f1f5a7fe91f59fdd6c8a90
Ray <rayc12079@gmail.com>
2024-06-11 21:28:33 -0400

    Updated accordion content

--- src/lib/getFaqs.js
+++ src/lib/getFaqs.js
@@ -1,6 +1,11 @@
 export default function getFaqs() {
   return [
     {
+      question: "What are rooms?",
+      answer:
+        "A room is a virtual space where a host sets a prompt on creation of the room. Users from around the world can view the room to share their guidance and insights in response.",
+    },
+    {
       question: "Do you gather any data or user information?",
       answer: "No, we do not store any data or personal information at all.",
     },
