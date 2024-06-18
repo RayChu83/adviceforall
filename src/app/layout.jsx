1ae1dc19f8b404fdfc170fbfaf9e844dfa3ad56f
Ray <rayc12079@gmail.com>
2024-06-18 07:19:39 -0400

    Added vercel insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import Navbar from "@/app/_components/Navbar";
 import Footer from "@/app/_components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         <Footer />
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
